e36ae568d95c5955d85039d9e8c8f5eb48c63ff3
kelseck <kelseyeckelberry@gmail.com>
2020-10-28 22:41:05 -0500

    Forecast Cards

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 var date = moment().add(10, 'days').calendar();
 
 $(document).ready(function() {
-var storeSearchedCity = "";
-})
+var storeSearchedCity = [];
+});
 
 $(".btn").on("click", function() {
     var value = $(".form-control").val();
     displayWeatherInfo(value);
-})
+    day1Forecast(value);
+    day2Forecast(value);
+    day3Forecast(value);
+    day4Forecast(value);
+    day5Forecast(value);
+});
 
 function displayWeatherInfo(searchedCity) {
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&units=imperial&appid=077cc081d0edde56ae0ece8d0be5b7df"
@@ -19,8 +24,8 @@ function displayWeatherInfo(searchedCity) {
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        
-        storeSearchedCity = localStorage.setItem("Past Search", searchedCity);
+        //console.log(response);
+        //storeSearchedCity = localStorage.setItem("Past Search", searchedCity);
         
         var tBody = $("tbody");
         var cityName = $("thead");
@@ -44,16 +49,205 @@ function displayWeatherInfo(searchedCity) {
         } else if (response.weather[0].main === "Snow") {
             var snow = $("<thead class='far fa-snowflake'>");
             icon.append(snow);
+        } else if (response.weather[0].main === "Mist") {
+            var mist = $("<thead class='fas fa-cloud-rain'>");
+            icon.append(mist);
         }
 
         tBody.append(temp, humid, wind);
         cityName.append(city);
        
     })
-    $("#data-view").prepend(cityName, tBody);
+    //$("#data-view").prepend(cityName, tBody);
+};
+
+function day1Forecast (searchedCity) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchedCity + "&units=imperial&appid=077cc081d0edde56ae0ece8d0be5b7df"
+
+    $('.forecast-date1').empty();
+    $('.forecast-icon1').empty();
+    $('.forecast-info1').empty();
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        //storeSearchedCity = localStorage.setItem("Past Search", searchedCity);
+        
+        var forecastDate = $(".forecast-date1");
+        var cardBody = $(".forecast-info1");
+
+        var day = response.list[0].dt_txt;
+        var temp = "Temp: " + response.list[0].main.temp + "°F";
+        var humid = "Humidity: " + response.list[0].main.humidity + "%";
+
+        if (response.list[0].weather[0].main === "Clouds") {
+            $("#forecast-icon1").addClass("fas fa-cloud");
+        } else if (response.list[0].weather[0].main === "Clear") {
+            $("#forecast-icon1").addClass("fas fa-sun");
+        } else if (response.list[0].weather[0].main === "Rain") {
+            $("#forecast-icon1").addClass("fas fa-cloud-showers-heavy");
+        } else if (response.list[0].weather[0].main === "Snow") {
+            $("#forecast-icon1").addClass("far fa-snowflake");
+        } else if (response.list[0].weather[0].main === "Mist") {
+            $("#forecast-icon1").addClass("fas fa-cloud-rain");
+        }
+
+        forecastDate.append(day);
+        cardBody.append(temp + "<br>" + humid);
+    })
+};
+
+function day2Forecast (searchedCity) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchedCity + "&units=imperial&appid=077cc081d0edde56ae0ece8d0be5b7df"
+
+    $('.forecast-date2').empty();
+    $('.forecast-icon2').empty();
+    $('.forecast-info2').empty();
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        //storeSearchedCity = localStorage.setItem("Past Search", searchedCity);
+        console.log(response);
+
+        var forecastDate = $(".forecast-date2");
+        var cardBody = $(".forecast-info2");
+
+        var day = response.list[6].dt_txt;
+        var temp = "Temp: " + response.list[6].main.temp + "°F";
+        var humid = "Humidity: " + response.list[6].main.humidity + "%";
+
+        if (response.list[6].weather[0].main === "Clouds") {
+            $("#forecast-icon2").addClass("fas fa-cloud");
+        } else if (response.list[6].weather[0].main === "Clear") {
+            $("#forecast-icon2").addClass("fas fa-sun");
+        } else if (response.list[6].weather[0].main === "Rain") {
+            $("#forecast-icon2").addClass("fas fa-cloud-showers-heavy");
+        } else if (response.list[6].weather[0].main === "Snow") {
+            $("#forecast-icon2").addClass("far fa-snowflake");
+        } else if (response.list[6].weather[0].main === "Mist") {
+            $("#forecast-icon2").addClass("fas fa-cloud-rain");
+        }
+
+        forecastDate.append(day);
+        cardBody.append(temp + "<br>" + humid);
+    })
+};
+
+function day3Forecast (searchedCity) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchedCity + "&units=imperial&appid=077cc081d0edde56ae0ece8d0be5b7df"
+
+    $('.forecast-date3').empty();
+    $('.forecast-icon3').empty();
+    $('.forecast-info3').empty();
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        //storeSearchedCity = localStorage.setItem("Past Search", searchedCity);
+        
+        var forecastDate = $(".forecast-date3");
+        var cardBody = $(".forecast-info3");
+
+        var day = response.list[14].dt_txt;
+        var temp = "Temp: " + response.list[14].main.temp + "°F";
+        var humid = "Humidity: " + response.list[14].main.humidity + "%";
+
+        if (response.list[14].weather[0].main === "Clouds") {
+            $("#forecast-icon3").addClass("fas fa-cloud");
+        } else if (response.list[14].weather[0].main === "Clear") {
+            $("#forecast-icon3").addClass("fas fa-sun");
+        } else if (response.list[14].weather[0].main === "Rain") {
+            $("#forecast-icon3").addClass("fas fa-cloud-showers-heavy");
+        } else if (response.list[14].weather[0].main === "Snow") {
+            $("#forecast-icon3").addClass("far fa-snowflake");
+        } else if (response.list[14].weather[0].main === "Mist") {
+            $("#forecast-icon3").addClass("fas fa-cloud-rain");
+        }
+
+        forecastDate.append(day);
+        cardBody.append(temp + "<br>" + humid);
+    })
+};
+
+function day4Forecast (searchedCity) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchedCity + "&units=imperial&appid=077cc081d0edde56ae0ece8d0be5b7df"
+
+    $('.forecast-date4').empty();
+    $('.forecast-icon4').empty();
+    $('.forecast-info4').empty();
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        //storeSearchedCity = localStorage.setItem("Past Search", searchedCity);
+        
+        var forecastDate = $(".forecast-date4");
+        var cardBody = $(".forecast-info4");
+
+        var day = response.list[22].dt_txt;
+        var temp = "Temp: " + response.list[22].main.temp + "°F";
+        var humid = "Humidity: " + response.list[22].main.humidity + "%";
+
+        if (response.list[22].weather[0].main === "Clouds") {
+            $("#forecast-icon4").addClass("fas fa-cloud");
+        } else if (response.list[22].weather[0].main === "Clear") {
+            $("#forecast-icon4").addClass("fas fa-sun");
+        } else if (response.list[22].weather[0].main === "Rain") {
+            $("#forecast-icon4").addClass("fas fa-cloud-showers-heavy");
+        } else if (response.list[22].weather[0].main === "Snow") {
+            $("#forecast-icon4").addClass("far fa-snowflake");
+        } else if (response.list[22].weather[0].main === "Mist") {
+            $("#forecast-icon4").addClass("fas fa-cloud-rain");
+        }
+
+        forecastDate.append(day);
+        cardBody.append(temp + "<br>" + humid);
+    })
+};
+
+function day5Forecast (searchedCity) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchedCity + "&units=imperial&appid=077cc081d0edde56ae0ece8d0be5b7df"
+
+    $('.forecast-date5').empty();
+    $('.forecast-icon5').empty();
+    $('.forecast-info5').empty();
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        //storeSearchedCity = localStorage.setItem("Past Search", searchedCity);
+        
+        var forecastDate = $(".forecast-date5");
+        var cardBody = $(".forecast-info5");
+
+        var day = response.list[30].dt_txt;
+        var temp = "Temp: " + response.list[30].main.temp + "°F";
+        var humid = "Humidity: " + response.list[30].main.humidity + "%";
+
+        if (response.list[30].weather[0].main === "Clouds") {
+            $("#forecast-icon5").addClass("fas fa-cloud");
+        } else if (response.list[30].weather[0].main === "Clear") {
+            $("#forecast-icon5").addClass("fas fa-sun");
+        } else if (response.list[30].weather[0].main === "Rain") {
+            $("#forecast-icon5").addClass("fas fa-cloud-showers-heavy");
+        } else if (response.list[30].weather[0].main === "Snow") {
+            $("#forecast-icon5").addClass("far fa-snowflake");
+        } else if (response.list[30].weather[0].main === "Mist") {
+            $("#forecast-icon5").addClass("fas fa-cloud-rain");
+        }
+
+        forecastDate.append(day);
+        cardBody.append(temp + "<br>" + humid);
+    })
 };
 
 function renderPastSearch() {
     pastSearch = localStorage.getItem("Past Search");
     displayWeatherInfo(searchedCity);
-}
+};
